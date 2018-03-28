bc8cee5d8d0609c1937e9130ef688826cfffd8e1
Berycoin Project <32966245+berycoin-project@users.noreply.github.com>
2018-03-28 19:35:06 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,7 +70,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         strNetworkID = "main";
-        consensus.nSubsidyHalvingInterval = 52631579; // 40 billion divided by 760
+        consensus.nSubsidyHalvingInterval = 160000000; // 40 billion divided by 760
         consensus.nMajorityEnforceBlockUpgrade = 750;
         consensus.nMajorityRejectBlockOutdated = 950;
         consensus.nMajorityWindow = 1000;
@@ -167,7 +167,7 @@ class CTestNetParams : public CChainParams {
 public:
     CTestNetParams() {
         strNetworkID = "test";
-        consensus.nSubsidyHalvingInterval = 52631579;
+        consensus.nSubsidyHalvingInterval = 160000000;
         consensus.nMajorityEnforceBlockUpgrade = 51;
         consensus.nMajorityRejectBlockOutdated = 75;
         consensus.nMajorityWindow = 100;
